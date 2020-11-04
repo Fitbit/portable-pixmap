e5c8ac5e79ca240bfe1bb03c8c9fbe6c23d6b4de
Liam McLoughlin <lmcloughlin@fitbit.com>
2020-11-04 12:31:43 +0000

    Add Prettier for code formatting
    Signed-off-by: Liam McLoughlin <lmcloughlin@fitbit.com>

--- jest.config.js
+++ jest.config.js
@@ -2,21 +2,13 @@
 
 module.exports = {
   transform: {
-    "^(?!.*\\.(d\\.ts)$).+\\.(js|ts)$": "ts-jest",
+    '^(?!.*\\.(d\\.ts)$).+\\.(js|ts)$': 'ts-jest',
   },
-  transformIgnorePatterns: [
-    "[/\\\\]node_modules[/\\\\].+\\.(js|mjs)$",
-  ],
-  moduleFileExtensions: [
-    "ts",
-    "js",
-  ],
-  testEnvironment: "node",
-  testRegex: "src/.*\\.test\\.ts$",
-  coverageDirectory: "<rootDir>/coverage",
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|mjs)$'],
+  moduleFileExtensions: ['ts', 'js'],
+  testEnvironment: 'node',
+  testRegex: 'src/.*\\.test\\.ts$',
+  coverageDirectory: '<rootDir>/coverage',
   collectCoverage: true,
-  collectCoverageFrom: [
-    "src/**/*.ts",
-    "!**/*.d.ts",
-  ],
+  collectCoverageFrom: ['src/**/*.ts', '!**/*.d.ts'],
 };
